0d8b4c1dc6d15824b3af011b2ed8ab42339ccd25
NadyaCodes <nadyacorscadden@gmail.com>
2024-09-12 23:45:35 -0400

    add some tissues terms

--- terms/mnemonics.ts
+++ terms/mnemonics.ts
@@ -8,5 +8,6 @@ export const mnemonics: ListObject = {
   // "Bones of the Hand": [["Please", "Make", "Cookies"], ["Phalanges", "Metacarpal bones", "Carpal bones"], "ordered"],
   // "Rotator Cuff Muscles": [["Sit", "In", "The", "Seat"], ["Supraspinatus", "Infraspinatus", "Teres Minor", "Subscapularis"], "unordered"],
   // "Systems of the Body": [["M", "U", "R", "D", "E", "R", "S", "L", "I", "N", "C"], ["Muscular", "Urinary", "Respiratory", "Digestive", "Endocrine", "Reproductive", "Skeletal", "Lymphatic", "Integumentary", "Nervous", "Cardiovascular"], "unordered"],
-  "4 Functions of Epithelial Tissue" : [["P", "A", "S", "S"], ["physical protection", "absorption", "sensation", "secretion"], "unordered"]
+  "4 Functions of Epithelial Tissue" : [["P", "A", "S", "S"], ["physical protection", "absorption", "sensation", "secretion"], "unordered"],
+  "Epithelial Tissue Layers" : [["Any", "BS", "is BLatant", "Ridiculousness"], ["apical surface", "basal surface", "basal lamina" , "reticular lamina"], "ordered"]
 }
